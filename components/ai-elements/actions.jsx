da1772521a5c3e2b978ca4f40d3f4ca21e4d866b
Prathap L <prathaplokesh04@gmail.com>
2025-09-29 20:39:32 +0530

    llm update added reasoning model

--- components/ai-elements/actions.jsx
+++ components/ai-elements/actions.jsx
@@ -30,7 +30,7 @@ export const Action = ({
   const button = (
     <Button
       className={cn(
-        "relative size-9 p-1 text-muted-foreground hover:text-foreground",
+        "relative size-9 p-1 left-4 text-muted-foreground hover:text-foreground",
         className
       )}
       size={size}
